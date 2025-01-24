c28a9f71d3329bcee13803487a63b05344dbe293
maxlukyrabbit <maxlukyrabbit@yandex.ru>
2025-01-24 19:36:37 +0300

    second

--- test_api/start_api/find_most.py
+++ test_api/start_api/find_most.py
@@ -1,8 +1,7 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from config import host, user, password_db, db_name
 import psycopg2
 import json
-import base64
 
 try:
     connection = psycopg2.connect(
@@ -16,31 +15,21 @@ except Exception as e:
     print(f"Ошибка подключения к базе данных: {e}")
     exit()
 
-
 class FindMost(Resource):
     def get(self):
         try:
             with connection.cursor() as cursor:
                 cursor.execute(
                     """
-                    SELECT * FROM lecture
+                    SELECT name, object_id, course FROM lecture
                     ORDER BY count_view DESC
                     """
                 )
                 rows = cursor.fetchall()
                 columns = [desc[0] for desc in cursor.description]
-
-                result = []
-                for row in rows:
-                    row_dict = dict(zip(columns, row))
-
-                    if row_dict.get('file_pdf'):
-                        row_dict['file_pdf'] = base64.b64encode(row_dict['file_pdf']).decode('utf-8')
-
-                    result.append(row_dict)
-
+                result = [dict(zip(columns, row)) for row in rows]
                 json_result = json.dumps(result, ensure_ascii=False, indent=4)
 
-            return json_result, 200
+            return json.loads(json_result), 200
         except Exception as e:
-            return {"message": f"Error retrieving lectures: {e}"}, 500
+            return {"message": f"Error retrieving data: {e}"}, 500
--- test_api/start_api/test.py
+++ test_api/start_api/test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:3000/api/find_all"
+url = "http://127.0.0.1:3000/api/find_most"
 
 response = requests.get(url)
 

897128f5699cb34839f0d1ed908ed9401de90ecf
maxlukyrabbit <maxlukyrabbit@yandex.ru>
2025-01-28 19:11:35 +0300

    get object and specialization

---
+++ test_api/start_api/get_specialization.py
@@ -0,0 +1,37 @@
+import json
+
+from flask_restful import Resource, reqparse
+from config import host, user, password_db, db_name
+import psycopg2
+
+try:
+    connection = psycopg2.connect(
+        host=host,
+        user=user,
+        password=password_db,
+        database=db_name
+    )
+    connection.autocommit = True
+except Exception as e:
+    print(f"Ошибка подключения к базе данных: {e}")
+    exit()
+
+
+class GetSpecialization(Resource):
+    def get(self):
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    SELECT id_specialization, specialization_name
+	                FROM specialization
+                    """
+                )
+                rows = cursor.fetchall()
+                columns = [desc[0] for desc in cursor.description]
+                result = [dict(zip(columns, row)) for row in rows]
+                json_result = json.dumps(result, ensure_ascii=False, indent=4)
+
+            return json.loads(json_result), 200
+        except Exception as e:
+            return {"message": f"Error retrieving data: {e}"}, 500
--- test_api/start_api/main.py
+++ test_api/start_api/main.py
@@ -4,6 +4,8 @@ from add_user import AddUser
 from add_lecture import AddLecture
 from find_most import FindMost
 from find_all import FindAll
+from get_specialization import GetSpecialization
+from get_object import GetObject
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +14,8 @@ api.add_resource(AddUser, "/api/user_add")
 api.add_resource(AddLecture, "/api/lecture_add")
 api.add_resource(FindMost, "/api/find_most")
 api.add_resource(FindAll, "/api/find_all")
+api.add_resource(GetSpecialization, "/api/get_specialization")
+api.add_resource(GetObject, "/api/get_object")
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000, host="127.0.0.1")
